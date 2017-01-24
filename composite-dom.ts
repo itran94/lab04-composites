2a685eec796ec50adfc49d2af514be390387f36f
Isabelle Tran <oohisabelle@yahoo.com>
2017-01-24 11:11:09 -0800

    working on lab

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,51 @@ interface IDomElement {
   print();
 }
 
+class TextNode implements IDomElement {
+  private text:string;
+  
+  constructor(text : string) {
+    this.text = text;
+  }
+
+  print() {
+    console.log(this.text)
+  }
+}
+
+class DomElement implements IDomElement {
+  
+  private texts:string;
+  private elements:IDomElement[];
+
+  constructor(texts:string) {
+    this.texts = texts;
+    this.elements = [];
+  }
+  
+  print() {
+    console.log();
+  }
+
+  add(texts) {
+    this.elements.push(texts);
+  }
+}
+
+/*
+interface Component {
+  doThis():void;
+}
+
+class Leaf implements Component {
+  doThis():void {}
+}
+
+class Composite implements Component {
+  addElement() {}
+  doThis():void {}
+}
+*/
+
+
+
